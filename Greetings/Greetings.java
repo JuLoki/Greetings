04e143746e7a70efd24cb2cacdb0d58b4ec60f69
Yuliya Lokshyna <loky27@gmail.com>
2022-10-09 16:46:13 +0300

    Initial commit

---
+++ Greetings/Greetings.java
@@ -0,0 +1,10 @@
+public class Greetings {
+    public static void main (String[] args){
+        System.out.println("Hello " + args[0]);
+        System.out.println("Hello " + args[1]);
+        System.out.println("Hello " + args[2]);
+        System.out.println("Hello " + args[3]);
+        System.out.println("Hello " + args[4]);
+    }
+
+}
---
+++ Main/Greetings.java
@@ -0,0 +1,10 @@
+public class Greetings {
+	public static void main(String[] args){
+		System.out.println("Hello " + args[0]);
+		System.out.print(" " + args[1]);
+		System.out.println(" " + args[2]);
+		System.out.println("   " + args[3]);
+		System.out.println("    " + args[4]);
+		System.out.println(" " + args[2]  + args[1]);
+		}
+}
